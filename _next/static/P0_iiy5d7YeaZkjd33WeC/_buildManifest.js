e90348fd5593a4133ff61a8195c9368922fa0fb1
sm1990 <sm1990@users.noreply.github.com>
2022-05-29 13:54:20 +0000

    Deploying to gh-pages from @ sm1990/sarani.github.io@d220a752c4f1c17693fb0ffe51a7ef8fdfdf66fe 🚀

--- _next/static/P0_iiy5d7YeaZkjd33WeC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/129-2745b459faf0d143.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-f04b78fe6871c946.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
