b649dce732888ec66445d31d2a1a04e46b970e6a
Sarani Mendis <saranimendisb@gmail.com>
2022-06-08 17:58:22 +0530

    update script

--- rearrangeFiles.sh
+++ rearrangeFiles.sh
@@ -4,4 +4,4 @@ cp -r ./out/learn/get-started/* ./learn
 #cp -r ./out/learn/references/* ./learn
 
 #Remove duplicated files
-rm -rf ./out/learn/get-started/ ./out/learn/guides/ ./out/learn/references/
+# rm -rf ./out/learn/get-started/ ./out/learn/guides/ ./out/learn/references/
