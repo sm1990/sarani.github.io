276f2022f79915efcae8feb4bb4960853e363b93
Sarani Mendis <saranimendisb@gmail.com>
2022-07-06 16:19:33 +0530

    add use case page

--- components/learn/use-cases/UseCases.js
+++ components/learn/use-cases/UseCases.js
@@ -19,53 +19,53 @@ export default function UseCases() {
         <Col xs={12} lg={4}>
           <div className={styles.langRow}>
             <a href={`${prefix}/learn/write-a-restful-api-with-ballerina/`} className={styles.linkText}>Write RESTful API</a>
-            <div className={styles.linkRow}>
+            {/* <div className={styles.linkRow}>
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(HTTP API Docs)</a>&nbsp;&nbsp;
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(Spec)</a>
-            </div>
+            </div> */}
           </div>
 
           <div className={styles.langRow}>
             <a href={`${prefix}/learn/write-a-grpc-service-with-ballerina/`} className={styles.linkText}>Write gRPC service</a>
-            <div className={styles.linkRow}>
+            {/* <div className={styles.linkRow}>
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(gRPC API Docs)</a>&nbsp;&nbsp;
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(Spec)</a>
-            </div>
+            </div> */}
           </div>
         </Col>
 
         <Col xs={12} lg={4}>
           <div className={styles.langRow}>
             <a href={`${prefix}/learn/write-a-graphql-api-with-ballerina/`} className={styles.linkText}>Write GraphQL API</a>
-            <div className={styles.linkRow}>
+            {/* <div className={styles.linkRow}>
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(GraphQL API Docs)</a>&nbsp;&nbsp;
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(Spec)</a>
-            </div>
+            </div> */}
           </div>
 
           <div className={styles.langRow}>
             <a href={`${prefix}/learn/work-with-data-using-queries-in-ballerina/`} className={styles.linkText}>Working with data using queries</a>
-            <div className={styles.linkRow}>
+            {/* <div className={styles.linkRow}>
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(Spec)</a>
-            </div>
+            </div> */}
           </div>
         </Col>
 
         <Col xs={12} lg={4}>
           <div className={styles.langRow}>
             <a href={`${prefix}/learn/build-a-data-service-in-ballerina/`} className={styles.linkText}>Work with databases</a>
-            <div className={styles.linkRow}>
+            {/* <div className={styles.linkRow}>
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(SQL API Docs)</a>&nbsp;&nbsp;
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(Spec)</a>
-            </div>
+            </div> */}
           </div>
         
           <div className={styles.langRow}>
             <a href={`${prefix}/learn/deploy-ballerina-on-kubernetes/`} className={styles.linkText}>Deploy on Kubernetes</a>
-            <div className={styles.linkRow}>
+            {/* <div className={styles.linkRow}>
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(Cloud API Docs)</a>&nbsp;&nbsp;
               <a href={`${prefix}/learn/language-basics/`} className={styles.linkSubText}>(Spec)</a>
-            </div>
+            </div> */}
           </div>
           </Col>
 
@@ -75,7 +75,7 @@ export default function UseCases() {
 
       <Row className="pageContentRow">
         <Col xs={12}>
-        <p>See more for standard library and other <a href={`${prefix}/learn/language-basics/`} className={styles.linkText}>use cases</a></p>
+        <p>See more for standard library and other <a href={`${prefix}/learn/use-cases/`} className={styles.linkText}>use cases</a></p>
         </Col>
       </Row>
     </>
