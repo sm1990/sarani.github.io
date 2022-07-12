7a548bdc66ec360e2d0bf6423835ee5bac53a35f
Sarani Mendis <saranimendisb@gmail.com>
2022-07-12 21:44:53 +0530

    fix mobile left nav & community sub menu

--- pages/community/index.js
+++ pages/community/index.js
@@ -9,30 +9,18 @@ import Slack from '../../components/community/slack/Slack';
 import TechTalk from '../../components/community/tech-talk/TechTalk';
 import GetInvolved from '../../components/community/get-involved/GetInvolved';
 import Contact from '../../components/community/contact/Contact';
+import SubMenu from '../../components/community/submenu/SubMenu';
 
 export default function Community() {
 
   return (
     <Layout>
       <Col sm={12}>
-        {/* <Row className="pageHeader pageContentRow communityRow">
-          <Col xs={12}>
 
-            
-            <div className="collapse navbar-collapse" id="myNavbar"> 
-               <ul className="nav navbar-nav secondary-nav" >
-                 <li className="active"><a href="#subscribe-to-our-newsletter">Newsletter</a></li>
-                 <li><a href="#events">Events</a></li>
-                 <li><a href="#resources">Resources</a></li>
-                 <li><a href="#ballerina-slack-community">Slack</a></li>
-                 <li><a href="#monthly-tech-talk">Tech Talk</a></li>
-                 <li><a href="#get-involved">Get involved</a></li>
-                 <li><a href="#contact-us">Contact us</a></li>
-               </ul>
-               
-             </div>
-          </Col>
-        </Row> */}
+        <Row className="pageContentRow communityRow communitySub">
+          <SubMenu />
+        </Row>
+
 
         <Row className="pageHeader pageContentRow communityRow">
           <Col xs={12}>
