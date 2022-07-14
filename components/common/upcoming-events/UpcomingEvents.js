b9a5f1b0a88646853a51b989c1846c7a8cd10056
Sarani Mendis <saranimendisb@gmail.com>
2022-07-14 17:15:40 +0530

    update content to match current site

--- components/common/upcoming-events/UpcomingEvents.js
+++ components/common/upcoming-events/UpcomingEvents.js
@@ -9,37 +9,37 @@ export default function UpcomingEvents() {
     <>
       <Row className={styles.eventRows}>
         <Col sm={12} md={2} className={styles.eventDateContainer}>
-          <p className={`${styles.eventDate} ${styles.eventDateNum}`}>May 25, 2022</p>	
-          <p className={styles.eventDate}>Wed., 6.30 p.m. PDT</p>	
-          <p className="eventLocation">Online</p>
+          <p className={`${styles.eventDate} ${styles.eventDateNum}`}>Aug 11, 2022</p>	
+          <p className={styles.eventDate}>Thurs., 3.00 p.m. PDT</p>	
+          <p className="eventLocation">Seattle, USA</p>
         </Col>
         <Col sm={12} md={7} className={styles.eventDetail} id="eventDetails">
-          <a target="_blank" href="https://www.meetup.com/sfjava/events/284669431" rel="noreferrer">	
-            <p className="eventName">Meetup</p>	
+          <a target="_blank" href="https://vslive.com/events/redmond-2022/home.aspx" rel="noreferrer">	
+            <p className="eventName">Visual Studio LIVE!</p>	
           </a>
-          <h5>Ballerina: A Language to Develop, Consume and Combine Network Services</h5>		
-          <a target="_blank" href="https://twitter.com/sameerajayasoma" rel="noreferrer">Sameera Jayasoma</a>, Senior Director, WSO2
+          <h5>VH19 Busy Developer&apos;s Guide to Ballerina</h5>		
+          <a target="_blank" rel="noreferrer" href="https://twitter.com/tedneward">Ted Neward</a>, Co-founder of Solidify
         </Col>
         <Col sm={12} md={3} className={styles.eventURL}>
-          <a className={styles.eventRegistration} href="https://www.womentech.net/speaker/Anupama/Pathirage/70302" target="_blank" rel="noreferrer">More info</a>
+          <a className={styles.eventRegistration} href="https://vslive.com/Events/Redmond-2022/Sessions/Thursday/VH19-Busy-Developers-Guide-to-Ballerina.aspx" target="_blank" rel="noreferrer">More info</a>
         </Col>
       </Row>
 
       <Row className={styles.eventRows}>
         <Col sm={12} md={2} className={styles.eventDateContainer}>
-          <p className={`${styles.eventDate} ${styles.eventDateNum}`}>May 25, 2022</p>	
-          <p className={styles.eventDate}>Wed., 6.30 p.m. PDT</p>	
-          <p className="eventLocation">Online</p>
+          <p className={`${styles.eventDate} ${styles.eventDateNum}`}>Sep 7, 2022</p>	
+          <p className={styles.eventDate}>Wed., 4.00 p.m. CDT</p>	
+          <p className="eventLocation">Austin, TX</p>
         </Col>
         <Col sm={12} md={7} className={styles.eventDetail} id="eventDetails">
-          <a target="_blank" href="https://www.meetup.com/sfjava/events/284669431" rel="noreferrer">	
-            <p className="eventName">Meetup</p>	
+          <a target="_blank" href="https://www.developerweek.com/global/conference/cloud/" rel="noreferrer">	
+            <p className="eventName">DeveloperWeek Cloud 2022</p>	
           </a>
-          <h5>Ballerina: A Language to Develop, Consume and Combine Network Services</h5>		
-          <a target="_blank" href="https://twitter.com/sameerajayasoma" rel="noreferrer">Sameera Jayasoma</a>, Senior Director, WSO2
+          <h5>Ballerina: Programming Language Designed for Cloud-Native Applications</h5>		
+          <a target="_blank" rel="noreferrer" href="https://twitter.com/sameerajayasoma">Sameera Jayasoma</a>, Senior Director, WSO2
         </Col>
         <Col sm={12} md={3} className={styles.eventURL}>
-          <a className={styles.eventRegistration} href="https://www.womentech.net/speaker/Anupama/Pathirage/70302" target="_blank" rel="noreferrer">More info</a>
+          <a className={styles.eventRegistration} href="https://www.developerweek.com/global/conference/cloud/schedule/" target="_blank" rel="noreferrer">More info</a>
         </Col>
       </Row>
     </>
